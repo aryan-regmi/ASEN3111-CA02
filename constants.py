f03d02cfc4e8a5edfb86e4e679ed75e22c79f4a6
Aryan Regmi <**>
2019-11-21 23:10:16 -0700

    Created  and populated constants file

---
+++ constants.py
@@ -0,0 +1,12 @@
+from math import radians
+
+c = 2       # Chord Length [m]
+
+alpha = 12      # AOA [degrees]
+alpha = radians(alpha)      # AOA [rad]
+
+V_inf = 68     # Freestream Velocity [m/s]
+
+P_inf = 101.3 * 10**3       # Freestream Pressure [Pa]
+
+rho_inf = 1.225     # Freestream Density [kg/m^3]
